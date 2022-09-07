2501ec434bed56c58726f5b9a3e81f87f3abd79c
kennedy <kennedyngosachanda@gmail.com>
2022-09-07 02:49:33 +0200

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
       
       <div className="d-flex flex-column justify-content-center align-items-center text-white hero px-5">
         <h5 className="">We Are Ready To Make Your Car Health Again</h5>
-        <h1 className="display-4 py-4">ZAMBIAN BEST CAR DOCTORS</h1>
+        <h1 className="display-4 py-4">CAR DOCTORS</h1>
         <p className=" h5">
           We are the best auto mechanic repair company. we offer hight quality services at an affordable price
         </p>
